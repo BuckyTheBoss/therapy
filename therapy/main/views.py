cf60bb1490c80053e3f285773ee9ecae39880159
oz kilim <ozkilim@hotmail.co.uk>
2019-04-28 12:21:27 +0100

    with base

--- therapy/main/views.py
+++ therapy/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile_edit(request):
+	return render(request, 'profile_edit.html')
+
+
+def doctor_profile_edit(request):
+	return render(request, 'doctor_profile_edit.html')
+
+
