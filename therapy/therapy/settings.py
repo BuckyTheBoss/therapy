7a39f1f2b5d05b67c346218e90176daf82fb8ff2
oz kilim <ozkilim@hotmail.co.uk>
2019-04-29 12:10:38 +0100

    logoutbutton

--- therapy/therapy/settings.py
+++ therapy/therapy/settings.py
@@ -124,11 +124,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'index'
 
 LOGIN_URL = '/login'
 
-LOGOUT_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/login'
 AUTH_USER_MODEL = "main.User"
 
 
