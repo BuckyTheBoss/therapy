9dc088d87b34fbe0c62cd4d5a1d759507a43b7bb
oz kilim <ozkilim@hotmail.co.uk>
2019-04-29 08:03:22 +0100

    changes monday morn

--- therapy/main/views.py
+++ therapy/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Therapist
 
 # Create your views here.
 def profile_edit(request):
@@ -8,8 +9,9 @@ def doctor_profile_edit(request):
 	return render(request, 'doctor_profile_edit.html')
 
 def doctor_profile(request):
-	'''this will have the initial chat space'''
-	return render(request, 'doctor_profile.html')
+	therapists = Therapist.objects.all()
+	'''this will have the initial chat space display this as a card'''
+	return render(request, 'doctor_profile.html', {'therapists' : therapists})
 
 def doctor_index(request):
 	'''this will have the initial chat space'''
@@ -17,6 +19,9 @@ def doctor_index(request):
 
 def index(request):
 	return render(request, 'index.html')
+
+
+
 
 
 
