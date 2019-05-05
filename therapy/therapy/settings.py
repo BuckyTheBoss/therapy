ec8af96e54be9636a3ed50f9dfede51e3bfda69e
Avi <buchman.avi@gmail.com>
2019-05-05 15:34:52 +0300

    settings fix

--- therapy/therapy/settings.py
+++ therapy/therapy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pu3s8jep0)z-u69jzp_b$xh((qp#tk1qk4isdi55vea8-_x)-s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['buckytheboss.pythonanywhere.com']
 
 
 # Application definition
